fda9046328a2e49776a1bf5ac88567a487f72146
zhangshengm <617256843@qq.com>
2020-05-18 23:01:20 +0800

    Update 225.py

--- 1_Stack_and_Queue/225.py
+++ 1_Stack_and_Queue/225.py
@@ -1,2 +1,39 @@
 # 队列实现栈
-#
+# First answer
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s1=[]
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.s1.append(x)
+        t=len(self.s1)
+        while t >1:
+            self.s1.append(self.s1.pop(0))
+            t=t-1
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.s1.pop(0)
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.s1[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return True if not self.s1 else False
+      
+# 队列实现栈
+# Second answer
