ad1e514b4a4a813c717ebafd7b28aaa1071b5edb
zhangshengm <617256843@qq.com>
2020-05-18 16:08:07 +0800

    Update 030.py

--- 1_Stack_and_Queue/030.py
+++ 1_Stack_and_Queue/030.py
@@ -1,1 +1,26 @@
+# First answer
+class MinStack:
 
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.s1=[]
+        self.minvalue=[]
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> None:
+        t=self.s1[-1]
+        del self.s1[-1]
+        return t
+
+    def top(self) -> int:
+        return self.s1[-1]
+
+    def min(self) -> int:
+        val=self.s1[0]
+        for i in self.s1:
+            if i < val:
+                val=i
+        return val
