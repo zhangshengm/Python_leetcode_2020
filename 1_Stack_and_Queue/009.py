56e594b44760000aa011c7b7769e293a3939e9fa
zhangshengm <617256843@qq.com>
2020-05-18 11:11:49 +0800

    Update 009.py

--- 1_Stack_and_Queue/009.py
+++ 1_Stack_and_Queue/009.py
@@ -1,1 +1,22 @@
+#First answer:
+#Execution time nearly：600 ms
+#Consumed memory nearly: 17.2 MB
+class CQueue:
 
+    def __init__(self):
+        self.s1=[]
+        self.s2=[]
+    def appendTail(self, value: int) -> None:
+        self.s1.append(value)
+
+
+    def deleteHead(self) -> int:
+        if len(self.s1)==0:
+            return -1
+        else:
+            self.s2.append(self.s1.pop(0))
+            return self.s2[-1]
+#Second answer:
+#Execution time nearly：600 ms
+#Consumed memory nearly: 17.2 MB
+#
