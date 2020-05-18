37316522d092a9549c5b15ec887a229906024572
zhangshengm <617256843@qq.com>
2020-05-18 20:47:13 +0800

    Update 232.py

--- 1_Stack_and_Queue/232.py
+++ 1_Stack_and_Queue/232.py
@@ -1,1 +1,32 @@
 
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s1=[]
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.s1.pop(0)
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.s1[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return False if self.s1 else True
