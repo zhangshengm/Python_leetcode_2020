2d7ae4a51db460327d7b9a03dc7b51ae6d913ed5
zhangshengm <617256843@qq.com>
2020-05-18 11:26:23 +0800

    Update 009.py

--- 1_Stack_and_Queue/009.py
+++ 1_Stack_and_Queue/009.py
@@ -1,6 +1,6 @@
 #First answer:
-#Execution time nearly：600 ms
-#Consumed memory nearly: 17.2 MB
+#Execution time nearly：624 ms
+#Consumed memory nearly: 17 MB
 class CQueue:
 
     def __init__(self):
@@ -14,9 +14,12 @@ class CQueue:
         if len(self.s1)==0:
             return -1
         else:
+            if len(self.s2)>=1:
+                self.s2.pop(0)
             self.s2.append(self.s1.pop(0))
-            return self.s2[-1]
+
+            return self.s2[0]
 #Second answer:
-#Execution time nearly：600 ms
-#Consumed memory nearly: 17.2 MB
+#Execution time nearly： ms
+#Consumed memory nearly:  MB
 #
