06df05f9131de62aa42917fc7bfa33e8c223743c
zhangshengm <617256843@qq.com>
2020-05-18 17:34:14 +0800

    Update 030.py

--- 1_Stack_and_Queue/030.py
+++ 1_Stack_and_Queue/030.py
@@ -16,7 +16,6 @@ class MinStack:
 
         del self.s1[-1]
     
-
     def top(self) -> int:
         return self.s1[-1]
 
@@ -26,3 +25,32 @@ class MinStack:
             if i < val:
                 val=i
         return val
+
+# Second answer
+#Execution time nearly：84 ms
+#Consumed memory nearly: 17 MB
+#思路如下: 1.考虑用额外栈存储最小值，把最小值元素放到栈顶（当最小值有重复值如何考虑？）
+#         2.原始栈在元素出栈时，会影响到额外栈（该元素是否为最小值）
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.s1=[]
+        self.minvalue=[]
+        
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+        if not self.minvalue or x <= self.minvalue[-1]:
+           self.minvalue.append(x)
+        
+    def pop(self) -> None:
+        if self.s1.pop() == self.minvalue[-1]:
+            self.minvalue.pop()
+
+    def top(self) -> int:
+        return self.s1[-1]
+
+    def min(self) -> int:
+        return self.minvalue[-1]
