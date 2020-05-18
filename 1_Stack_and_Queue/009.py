8296f895ff3205ec86c81b811192ecd67c30f3c7
zhangshengm <617256843@qq.com>
2020-05-18 11:54:04 +0800

    Update 009.py

--- 1_Stack_and_Queue/009.py
+++ 1_Stack_and_Queue/009.py
@@ -16,6 +16,23 @@ class CQueue:
             self.s2.append(self.s1.pop(0))
             return self.s2.pop()
 #Second answer:
-#Execution time nearly： ms
-#Consumed memory nearly:  MB
-#
+#Execution time nearly： 550 ms
+#Consumed memory nearly:  16.8 MB
+class CQueue:
+
+    def __init__(self):
+        self.s1=[]
+        self.s2=[]
+    def appendTail(self, value: int) -> None:
+        self.s1.append(value)
+
+
+    def deleteHead(self) -> int:
+        if self.s2:
+            return self.s2.pop()
+        if len(self.s1)==0:
+            return -1
+        else:
+            while self.s1:
+               self.s2.append(self.s1.pop())
+            return self.s2.pop()
