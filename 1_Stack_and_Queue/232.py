da191b540f307ec905299f227d73c696b2f6b997
zhangshengm <617256843@qq.com>
2020-05-18 22:20:28 +0800

    Update 232.py

--- 1_Stack_and_Queue/232.py
+++ 1_Stack_and_Queue/232.py
@@ -1,6 +1,6 @@
-#First answer:
-#Execution time nearly：40 ms
-#Consumed memory nearly: 13.7 MB
+#栈实现队列
+#（1）判定栈为空的条件
+#（2）考虑返回栈顶元素 peek()、出栈函数pop()以及push()函数先后顺序，都要将元素转移到第二个数组
 class MyQueue:
 
     def __init__(self):
@@ -8,7 +8,7 @@ class MyQueue:
         Initialize your data structure here.
         """
         self.s1=[]
-
+        self.s2=[]
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
@@ -19,16 +19,28 @@ class MyQueue:
         """
         Removes the element from in front of queue and returns that element.
         """
-        return self.s1.pop(0)
-
+#         if self.empty():
+#             return
+        if not self.s2:
+           while self.s1:
+               self.s2.append(self.s1.pop())
+           return self.s2.pop()
+        else:
+           return self.s2.pop()
     def peek(self) -> int:
         """
         Get the front element.
         """
-        return self.s1[0]
-
+#         if self.empty():
+#             return
+        if not self.s2:
+           while self.s1:
+               self.s2.append(self.s1.pop())
+           return self.s2[-1]
+        else:
+           return self.s2[-1]
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
-        return False if self.s1 else True
+        return True if (not self.s2) and (not self.s1) else False
