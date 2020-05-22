91eb93d2248fc12be93be6af7f0abcffdfb979c4
zhangshengm <617256843@qq.com>
2020-05-22 11:15:51 +0800

    Create 04二维数组中的查找.py

---
+++ 剑指offer/04二维数组中的查找.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        else:
+            if not matrix[0]:
+                return False
+        t1=len(matrix)
+        t2=len(matrix[0])
+        # if t2==0:
+        #    return False
+        j=t2-1
+        s1=[]
+        for i in range(t1):
+            if matrix[i][j]==target or matrix[i][0]==target:
+                return True
+            if matrix[i][j]>target and matrix[i][0]<target:
+                s1.append(i)
+        #列号
+        for t in range(1,j):
+            for i in s1:
+                if matrix[i][t]==target:
+                    return True
+        return False    
+        
+        
+        
