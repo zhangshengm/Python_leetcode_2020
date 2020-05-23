4b4125fe98e8651bb18bea78a6c2a1a0657327e9
zhangshengm <617256843@qq.com>
2020-05-23 11:18:22 +0800

    Update 05.py

--- 剑指offer/05.py
+++ 剑指offer/05.py
@@ -23,3 +23,13 @@ class Solution:
             else:
                 s1.append(i)
        return ''.join(s1)
+
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+       s1=''
+       for i in s:
+            if i==' ':
+                s1+='%20'
+            else:
+                s1+=i
+       return s1
