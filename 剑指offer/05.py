d8eabdf05ab389200134fab1319772d236b01cdf
zhangshengm <617256843@qq.com>
2020-05-23 11:05:55 +0800

    Update 05.py

--- 剑指offer/05.py
+++ 剑指offer/05.py
@@ -9,3 +9,17 @@ class Solution:
                 s1.append(s[i])
             i+=1
         return ''.join(s1)
+    
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+        return ''.join('%20' if c==' ' else c for c in s)
+
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+       s1=[]
+       for i in s:
+            if i==' ':
+                s1.append('%20')
+            else:
+                s1.append(i)
+       return ''.join(s1)
