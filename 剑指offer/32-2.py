fbd92a0e5ac4638dd6e5dcc26d5b9fddab45c893
zhangshengm <617256843@qq.com>
2020-05-26 21:00:46 +0800

    Update 32-2.py

--- 剑指offer/32-2.py
+++ 剑指offer/32-2.py
@@ -4,14 +4,13 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         s1=[]
         #如果树为空，返回空列表
         if not root: return s1
         #注意考虑节点和节点的值得区别
-        s1.append([root.val])
+        # s1.append([root.val])
         s2=[root]
         while s2:
               #存每一层的节点
@@ -19,13 +18,33 @@ class Solution:
               t1=[]
               #遍历每一层节点
               for node in s2:
+                 t1.append(node.val)
                  if node.left: 
                     t.append(node.left)
-                    t1.append(node.left.val)
                  if node.right:
-                   t.append(node.right)
-                   t1.append(node.right.val)
-              if t1:
-                 s1.append(t1)
+                    t.append(node.right)
+              s1.append(t1)
               s2=t
+        return s1
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+      
+        s1=[]
+        #如果树为空，返回空列表
+        if not root: return s1
+        #注意考虑节点和节点的值得区别
+        s2=deque()
+        s2.append(root)
+        while s2:
+            t2=[]
+            num=len(s2)
+            for i in range(num):
+               node=s2.popleft()
+               t2.append(node.val)
+               if node.left: 
+                  s2.append(node.left)
+               if node.right: 
+                  s2.append(node.right)
+            s1.append(t2)
         return s1
