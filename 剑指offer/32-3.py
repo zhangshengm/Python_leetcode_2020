96a9edebf176676cbfcce0bc30f3380c3b373ed6
zhangshengm <617256843@qq.com>
2020-05-26 21:04:13 +0800

    Update 32-3.py

--- 剑指offer/32-3.py
+++ 剑指offer/32-3.py
@@ -7,30 +7,23 @@
 
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        s1=[]
+        flag,s1,s2=0,[],[root]
         #如果树为空，返回空列表
         if not root: return s1
         #注意考虑节点和节点的值得区别
-        s1.append([root.val])
-        s2=[root]
-        flag=1
+        # s1.append([root.val])
         while s2:
               #存每一层的节点
-              t=[]
-              t1=[]
+              t,t1=[],[]
               flag+=1
               #遍历每一层节点
               for node in s2:
-                   if node.left: 
-                     t.append(node.left)
-                     t1.append(node.left.val)
-                   if node.right:
-                     t.append(node.right)
-                     t1.append(node.right.val)
+                   t1.append(node.val)
+                   if node.left:  t.append(node.left)      
+                   if node.right: t.append(node.right)
               if t1 and flag%2==0:
-                 t1.reverse()
-                 s1.append(t1)
-              if t1 and flag%2==1:
-                 s1.append(t1)
+                   t1.reverse() 
+                   s1.append(t1)
+              if t1 and flag%2==1: s1.append(t1)
               s2=t
         return s1
