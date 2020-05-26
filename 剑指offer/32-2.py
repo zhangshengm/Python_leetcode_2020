65ffe06757f406e8bc7e649d6108dffe8080ba40
zhangshengm <617256843@qq.com>
2020-05-26 20:26:47 +0800

    Create 32-2.py

---
+++ 剑指offer/32-2.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        s1=[]
+        #如果树为空，返回空列表
+        if not root: return s1
+        #注意考虑节点和节点的值得区别
+        s1.append([root.val])
+        s2=[root]
+        while s2:
+              #存每一层的节点
+              t=[]
+              t1=[]
+              #遍历每一层节点
+              for node in s2:
+                 if node.left: 
+                    t.append(node.left)
+                    t1.append(node.left.val)
+                 if node.right:
+                   t.append(node.right)
+                   t1.append(node.right.val)
+              if t1:
+                 s1.append(t1)
+              s2=t
+        return s1
