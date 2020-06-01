a2347cbcd7c3a8de7d92987bed5b8c348743da66
zhangshengm <617256843@qq.com>
2020-06-01 10:57:46 +0800

    Update 24.py

--- 剑指offer/24.py
+++ 剑指offer/24.py
@@ -19,8 +19,35 @@ class Solution:
             if len(s1)==0:  node.next=None
             #其余节点指向下一个节点
             else:   node.next=s1[-1]
-            #保存第一个节点
+            ##建立链接关系之后，再保存第一个节点
             if flag==0: 
                 t.next=node.next
                 flag=1
         return t
+#递归
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        def helper(before,now):
+            #before为尾结点
+            if now==None: return before
+            #保存下一个节点
+            node=now.next
+            #改变当前节点链接
+            now.next=before
+            return helper(now,node)
+        return helper(None,head)
+    
+#建立双指针，一边遍历，一边链接
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head: return head
+        cur=head
+        pre=None
+        while cur:
+            #新建一个节点
+            temp=ListNode(cur.val)
+            #建立指向关系
+            temp.next=pre
+            pre=temp
+            cur=cur.next
+        return temp
