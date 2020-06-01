3e3b9b77b0650c78053af4816ea9e6e6feb7afe5
zhangshengm <617256843@qq.com>
2020-06-01 19:52:19 +0800

    Update 18.py

--- 剑指offer/18.py
+++ 剑指offer/18.py
@@ -1,25 +1,21 @@
 
 class Solution:
     def deleteNode(self, head: ListNode, val: int) -> ListNode:
-        flag,First,t=0,head,None
+        flag,First=0,head
         if not head: return head
         #如果删除的是头节点
         if val==head.val:
             First=First.next
             return First
+        #保存头结点
         t=First
-        #遍历每个节点
         while First:
-              #保存当前节点
               before=First
-              #取到下一个节点
               after=First.next
               #删除节点
               if val==after.val:
-                 after=after.next
-                 before.next=after
+                 before.next=after.next
                  return t
-              #遍历节点
               First=First.next
               #保存首节点
               if flag==0:
