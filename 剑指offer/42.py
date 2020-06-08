2a42df4ae05c8764503092037f7f6fd7c7bab288
zhangshengm <617256843@qq.com>
2020-06-08 11:38:35 +0800

    Create 42.py

---
+++ 剑指offer/42.py
@@ -0,0 +1,21 @@
+class Solution:
+    #考虑两种情况:数组全为负数；数组有正数和负数
+    def maxSubArray(self, nums: List[int]) -> int:
+        i,j,s,s1=0,0,0,[]
+        if not nums: return 
+        while i <=len(nums)-1 and j<=len(nums)-1:
+            s+=nums[j]
+            #前面数组之和为正数，则继续寻找最大值
+            if s>=0:
+               j+=1
+               if not s1 or s>s1[-1]:
+                 s1.append(s) 
+            #前面数组之和为负数，则重新选取第一个数的下标
+            else:
+               s=0
+               i+=1
+               j=i
+        #如果为空，说明全为负数，取出最大值即可
+        if not s1:
+            return max(nums)
+        return s1[-1]
