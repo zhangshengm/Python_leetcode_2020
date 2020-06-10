e6127374d29973e03d1d85f7e4078b4363c7f07e
zhangshengm <617256843@qq.com>
2020-06-10 10:40:19 +0800

    Update 42.py

--- 剑指offer/42.py
+++ 剑指offer/42.py
@@ -19,3 +19,17 @@ class Solution:
         if not s1:
             return max(nums)
         return s1[-1]
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #f(i)=nums[i], if i=0 or f(i-1)<=0
+        #f(i)=f(i-1)+nums[i], if f(i-1)>0
+        maxv,sumv=float('-inf'),0
+        for i in range(len(nums)):
+            if sumv<=0:
+                sumv=nums[i]
+            else:
+                sumv+=nums[i]
+            if sumv>maxv:
+                maxv=sumv
+        return maxv
