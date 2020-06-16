0212a005f38d430cbde5df00454c489b547f9c52
zhangshengm <617256843@qq.com>
2020-06-16 21:54:39 +0800

    Update 50.py

--- 分而治之/50.py
+++ 分而治之/50.py
@@ -10,3 +10,15 @@ class Solution:
             return y*y if N%2==0 else y*y*x
      
         return quickpow(n) if n>0 else 1.0/quickpow(-n)
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x==0.0: return 0.0
+        res=1
+        if n<0: x,n=1/x,-n
+        while n:
+            #将奇数项的x乘到res项
+            if n%2==1: res*=x
+            x*=x
+            n//=2
+        return res
