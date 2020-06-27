3784c58fd2a0737897efdf2ecef97d94ce977c79
zhangshengm <617256843@qq.com>
2020-06-27 17:23:04 +0800

    Update 198.py

--- 动态规划/198.py
+++ 动态规划/198.py
@@ -10,3 +10,16 @@ class Solution:
         for i in range(3,len(nums)+1):    
             dp[i]=max(dp[i-2],dp[i-3])+nums[i-1]
         return max(dp)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:return 0
+        if len(nums)==1: return nums[0]
+        #dp[i]代表前i个元素的最大累加值
+        dp=[0]*(len(nums)+1)
+        dp[0]=0
+        dp[1]=nums[0]
+        dp[2]=max(nums[1],nums[0])
+        for i in range(3,len(nums)+1):    
+            dp[i]=max(dp[i-2]+nums[i-1],dp[i-1])
+        return max(dp)
