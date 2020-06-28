86fe51d06211741ca9687ee3e171f58149374c4b
zhangshengm <617256843@qq.com>
2020-06-28 17:33:17 +0800

    Update 123.py

--- 动态规划/123.py
+++ 动态规划/123.py
@@ -22,3 +22,23 @@ class Solution:
        for j in range(num-1):
            t=max(t,profit1[j]+profit2[j+1])
        return t
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices)<=1: return 0
+        dp=[[[0,0] for _ in range(3)] for _ in range(len(prices))]
+        #j代表交易次数
+        #dp[i][j][0]代表第i+1天没有持有股票的利润
+        #dp[i][j][1]代表第i+1天持有股票的利润
+        for i in range(len(prices)):
+            dp[i][0][0]=0
+            dp[i][0][1]=-float('inf')
+        for j in range(1,3):
+            dp[0][j][0]=0
+            dp[0][j][1]=-prices[0]
+
+        for i in range(1,len(prices)):
+            for j in range(1,3):
+              dp[i][j][0]=max(dp[i-1][j][0],dp[i-1][j][1]+prices[i])
+              dp[i][j][1]=max(dp[i-1][j][1],dp[i-1][j-1][0]-prices[i])
+        return dp[-1][-1][0]
