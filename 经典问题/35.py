0b8937f41555dc5d231c3d045d2db5429621aa59
zhangshengm <617256843@qq.com>
2020-06-29 11:05:29 +0800

    Create 35.py

---
+++ 经典问题/35.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i,j=0,len(nums)-1
+        while i<=j:
+              m=(i+j)//2
+              if nums[m]>target:   j=m-1
+              elif nums[m]<target: i=m+1
+              else:
+                  return m
+        return i
