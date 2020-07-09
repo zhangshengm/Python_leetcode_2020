a840876d49114bd3e858482d7d2e9a22a6d7633d
zhangshengm <617256843@qq.com>
2020-07-09 10:06:17 +0800

    Update 16.py

--- 剑指offer/16.py
+++ 剑指offer/16.py
@@ -5,8 +5,10 @@ class Solution:
         #保存奇数次方多余
         res=1
         while t:
+            # t&1 
             if t%2==1:
                res*=x
             x*=x
+            # t>>=1,向右移动一位，删除最后一位
             t//=2
         return res if n>0 else 1/res
