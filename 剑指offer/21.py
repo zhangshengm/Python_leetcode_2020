4ed95c35b71c4a299c873cae4f2db556400f042c
zhangshengm <617256843@qq.com>
2020-07-09 11:02:24 +0800

    Update 21.py

--- 剑指offer/21.py
+++ 剑指offer/21.py
@@ -9,3 +9,23 @@ class Solution:
             else:
                 t2.append(i)
         return t1+t2
+
+class Solution:
+    # 时间复杂度: O(n)
+    #空间复杂度: O(1)
+    def exchange(self, nums: List[int]) -> List[int]:
+        #首尾双指针
+        i,j=0,len(nums)-1
+        while i<j:
+            #如果前为
+            if nums[i]%2==0 and nums[j]%2!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+                j-=1
+            if nums[i]%2!=0:
+                i+=1
+            if nums[j]%2==0:
+                j-=1
+     
+        return nums
+            
