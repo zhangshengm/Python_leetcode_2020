4c1499b6e7e1f7b376a5aa69b8b22ea6aad067ef
zhangshengm <617256843@qq.com>
2020-07-10 21:10:37 +0800

    Update 39.py

--- 剑指offer/39.py
+++ 剑指offer/39.py
@@ -15,3 +15,17 @@ class Solution:
                 t[i]+=1
                 if t[i]>(len(nums)//2): return i
                 
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        #给众数设置为1票,其他数为-1票
+        vote=0
+        for i in nums:
+            #当投票数归零时，x设置为众数
+            if vote==0:  x=i
+            #如果遇到其他数，投票数减一
+            if i!=x:
+                vote-=1
+            #如果遇到其他数，投票数加一
+            else:
+                vote+=1
+        return x 
