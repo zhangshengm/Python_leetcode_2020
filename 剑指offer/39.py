a99901689274d168fea4c2ee654cce8b5af2bd28
zhangshengm <617256843@qq.com>
2020-07-10 19:01:00 +0800

    Update 39.py

--- 剑指offer/39.py
+++ 剑指offer/39.py
@@ -4,4 +4,14 @@ class Solution:
         nums.sort()
         return nums[t]
         
-        
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        if len(nums)==1: return nums[0]
+        t={}
+        for i in nums:
+            if i not in t:
+                t[i]=1
+            else:
+                t[i]+=1
+                if t[i]>(len(nums)//2): return i
+                
