715eedeae3e55786d06fb8447df57f8110e5067d
zhangshengm <617256843@qq.com>
2020-07-14 15:59:41 +0800

    Update 48.py

--- 剑指offer/48.py
+++ 剑指offer/48.py
@@ -33,3 +33,22 @@ class Solution:
                   break
                res=max(res,len(maxs))
         return res
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s)<=1: return len(s)
+        head,res=0,1
+        #保存某个元素最近出现的位置
+        hashmap={}
+        for i in range(len(s)):
+            #如果元素重复，更新头部指针指向首个重复元素的后一个元素
+            #max(head,hashmap[s[i]]+1)保证head一直往后
+            if s[i] in hashmap:
+                head=max(head,hashmap[s[i]]+1)
+            #    head=max(hashmap[s[i]],head)
+
+            #更新当前字符最近出现的位置，当前坐标为i
+            hashmap[s[i]]=i
+
+            #从第head个字符到第i个字符均为不重复子串
+            res=max(res,i-head+1)
+        return res
