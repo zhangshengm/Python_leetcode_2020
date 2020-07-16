abf3853113a5037397bb358359e5f3eeeb7ce48a
zhangshengm <617256843@qq.com>
2020-07-16 17:06:06 +0800

    Update 53-2.py

--- 剑指offer/53-2.py
+++ 剑指offer/53-2.py
@@ -6,3 +6,17 @@ class Solution:
                 return i
         #如果没找到，则缺失值为最大的值
         return i+1
+    
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        i,j=0,len(nums)-1
+        while i<=j:
+            mid=(i+j)//2
+            #如果数组某位置的值等于该位置，说明缺失值在位置之后
+            if nums[mid]==mid: i=mid+1
+            #如果数组某位置的值等于该位置，说明缺失值在位置之前
+            else:
+                j=mid-1
+        
+        return j+1
+        
