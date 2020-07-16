53d1555896c2b82c5591828c27528aec8a9e7392
zhangshengm <617256843@qq.com>
2020-07-16 16:09:19 +0800

    Update 53.py

--- 剑指offer/53.py
+++ 剑指offer/53.py
@@ -10,3 +10,16 @@ class Solution:
             return hashmap[target]
         else:
             return 0
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def findtarget(nums,target):
+            i,j=0,len(nums)-1
+            while i<=j:
+              mid=(i+j)//2
+              if nums[mid]<=target:
+                  i=mid+1
+              else: j=mid-1
+            return i
+
+        return findtarget(nums,target)-findtarget(nums,target-1)
