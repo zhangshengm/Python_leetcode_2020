01b5c2c3c1ec1854a7803f2f03f0537f418cdb95
zhangshengm <617256843@qq.com>
2020-07-16 16:40:44 +0800

    Update 53-2.py

--- 剑指offer/53-2.py
+++ 剑指offer/53-2.py
@@ -1,6 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        #存在的值和坐标的关系是: nums[i]=i
         for i in range(len(nums)):
             if nums[i]!=i:
                 return i
+        #如果没找到，则缺失值为最大的值
         return i+1
