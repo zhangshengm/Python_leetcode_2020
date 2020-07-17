f380cb35d1c58365b05e2adf8e5bc78e16bb5b35
zhangshengm <617256843@qq.com>
2020-07-17 18:07:48 +0800

    Update 58-2.py

--- 剑指offer/58-2.py
+++ 剑指offer/58-2.py
@@ -3,6 +3,7 @@ class Solution:
     def reverseLeftWords(self, s: str, n: int) -> str:
     return s[n:]+s[:n]
 
+#join函数列表转list
 class Solution:
     def reverseLeftWords(self, s: str, n: int) -> str:
         res=[]
@@ -11,4 +12,14 @@ class Solution:
         for j in range(0,n):
              res.append(s[j])
         return "".join(res)
+        
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        res=[]
+        for i in range(n,n+len(s)):
+            res.append(s[i%len(s)])
+          
+        return "".join(res)
+        
+
         
