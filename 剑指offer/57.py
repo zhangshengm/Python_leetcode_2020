5f337f105e7de06ff9c42b6bb7c9a57ba371fb10
zhangshengm <617256843@qq.com>
2020-07-17 11:43:05 +0800

    Update 57.py

--- 剑指offer/57.py
+++ 剑指offer/57.py
@@ -18,5 +18,17 @@ class Solution:
         return False
 
 
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i,j=0,len(nums)-1
+        while i<=j:
+              if nums[j]>target:
+                 j-=1
+              else:
+                 if nums[i]+nums[j]==target:
+                     return [nums[i],nums[j]]
+                 elif nums[i]+nums[j]>target:
+                     j-=1
+                 else:
+                     i+=1
 
