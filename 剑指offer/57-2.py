da841ab751c496c2f543b01d4704ff9efb35166d
zhangshengm <617256843@qq.com>
2020-07-17 17:47:01 +0800

    Update 57-2.py

--- 剑指offer/57-2.py
+++ 剑指offer/57-2.py
@@ -17,7 +17,26 @@ class Solution:
                   j=i+1
         return res
         
-
+class Solution:
+    def findContinuousSequence(self, target: int) -> List[List[int]]:
+        t,res=target//2+1,[]
+        i,j,tmp=1,2,1
+        while i<t and j<=t:
+              #如果当前累加值小于target,j往后移动
+              if tmp+j<target:
+                 tmp+=j
+                 j+=1
+              elif tmp+j==target:
+                  #如果累加值等于target,将结果放入res列表中
+                  res.append(list(range(i,j+1)))
+                  #更新累加值，减少计算量,减去i
+                  tmp-=i
+                  i+=1
+              else: 
+                  #如果累加值大于target,更新i,tmp
+                  tmp-=i
+                  i+=1
+        return res
                  
                  
         
