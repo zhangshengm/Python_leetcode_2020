71a4090fcd2ba6663498744aba0a7b57d05a63ae
zhangshengm <617256843@qq.com>
2020-07-20 10:09:09 +0800

    Update 136.py

--- 数组/136.py
+++ 数组/136.py
@@ -7,3 +7,12 @@ class Solution:
             else:
                 hashm[nums[i]]+=1
         return list((hashm.keys()))[list(hashm.values()).index(1)]
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        #任何数和0异或是数字本身
+        #任何数与自身异或为0
+        #异或满足交换律和结合律
+        t=0
+        for i in range(len(nums)):
+            t^=nums[i]
+        return t
