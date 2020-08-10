f1823b34f38a9cebfe4cb327b49e74d80d407ba8
zhangshengm <617256843@qq.com>
2020-08-10 17:08:40 +0800

    Update 123.py

--- 动态规划/123.py
+++ 动态规划/123.py
@@ -42,3 +42,29 @@ class Solution:
               dp[i][j][0]=max(dp[i-1][j][0],dp[i-1][j][1]+prices[i])
               dp[i][j][1]=max(dp[i-1][j][1],dp[i-1][j-1][0]-prices[i])
         return dp[-1][-1][0]
+    
+class Stock:
+    def maxProfit(self, prices, n):
+        #正序，当前值-最小值,
+        #逆序，最大值-当前值
+        if n<=1:
+           return 0
+        res_1,res_2=[0]*(n),[0]*(n)
+        tmp1,tmp2=prices[0],prices[-1]
+        m1,m2=0,0
+        for i in range(1,n):
+            if prices[i]<=tmp1:
+               tmp1=prices[i]
+            else:
+               m1=max(m1,prices[i]-tmp1)
+               res_1[i]=m1
+        for j in range(n-2,-1,-1):
+            if prices[j]>=tmp2:
+               tmp2=prices[j]
+            else:
+               m2=max(m2,tmp2-prices[j])
+               res_2[j]=m2
+        t=max(res_1[-1],res_2[0])
+        for k in range(0,n-1):
+            t=max(t,res_1[k]+res_2[k+1])
+        return t
