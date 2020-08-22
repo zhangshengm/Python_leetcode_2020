e1bfd936f03168f78848be73e307650a8d7a6dff
zhangshengm <617256843@qq.com>
2020-08-22 19:02:40 +0800

    Update 92.py

--- 面试题/92.py
+++ 面试题/92.py
@@ -38,3 +38,33 @@ class Solution:
         tmp.next=after
         return a.next
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+        a=ListNode(-1)
+        a.next=head
+        #before为m-1个节点
+        b=a
+        for i in range(m):
+            before=b
+            b=b.next
+        # #此时b为m对应的节点
+        tmp=b
+        node1,after=None,None
+        for j in range(n-m+1):
+            #下一个节点
+            after=tmp.next
+            #当前节点指向上一个节点
+            tmp.next=node1
+            #上一个节点等于当前节点
+            node1=tmp
+            #当前节点等于下一个节点
+            tmp=after
+        before.next=node1
+        b.next=after
+        return a.next
