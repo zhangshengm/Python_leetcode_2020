8a240800911427083eb1843c0152abd9bc69404d
zhangshengm <617256843@qq.com>
2020-08-24 10:51:02 +0800

    Update 04.01.py

--- 图/04.01.py
+++ 图/04.01.py
@@ -1,6 +1,6 @@
 
     #构造有向的邻接矩阵，L[node]=[邻接节点]
-    #首先考虑自环情况，自环情况仅影响start=end情况，然后删除自环的节点
+    #首先考虑自环,平行边情况，用h=set()保存res中添加过邻接节点的情况
     #广度优先遍历res=L[start]，查看target是否在res中，不在则pop出节点t，res新加入L[t]继续判断
 class Solution:
     def findWhetherExistsPath(self, n: int, graph: List[List[int]], start: int, target: int) -> bool:
