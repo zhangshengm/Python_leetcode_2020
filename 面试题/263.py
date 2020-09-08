1eb3ad8d9a2c2a5d2eddfaef98ef25fc6657bd7d
zhangshengm <617256843@qq.com>
2020-09-08 23:01:17 +0800

    Update 263.py

--- 面试题/263.py
+++ 面试题/263.py
@@ -11,3 +11,18 @@ class Solution:
         return num==1
         
         
+class Solution:
+    def isUgly(self, num: int) -> bool:
+        t=[2,3,5]
+        #判断能否被2,3,5整除
+        def divide_t(n): 
+           for i in range(len(t)):
+               if n%t[i]==0:
+                  return True
+           return False
+        #如果大于1，且能被整除
+        while num>1 and divide_t(num):
+            for i in range(len(t)):
+                if num%t[i]==0:
+                   num=num/t[i]
+        return num==1
