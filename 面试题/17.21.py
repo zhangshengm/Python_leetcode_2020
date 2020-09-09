cd4af49a6890c7f0357838ec1111179e8d12ac05
zhangshengm <617256843@qq.com>
2020-09-09 22:31:22 +0800

    Update 17.21.py

--- 面试题/17.21.py
+++ 面试题/17.21.py
@@ -4,14 +4,34 @@ class Solution:
         if len(height)<3: return 0
         res=0
         for i in range(1,len(height)-1):
-            if max(height[:i])<max(height[i+1:]):
-               if max(height[:i])>height[i]:
-                  res+=max(height[:i])-height[i]
+            tmp_left=max(height[:i])
+            tmp_right=max(height[i+1:])
+            if tmp_left<tmp_right:
+               if tmp_left>height[i]:
+                  res+=tmp_left-height[i]
             else:
-               if  max(height[i+1:])>height[i]:
-                   res+=max(height[i+1:])-height[i]
+               if  tmp_right>height[i]:
+                   res+=tmp_right-height[i]
         return res       
+        
+                    
 
-
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        #中低高，或者高低中
+        if not height: return 0
+        res=0
+        le,ri=0,len(height)-1
+        left_max,right_max=height[0],height[ri]
+        while le<ri:
+            if left_max<=right_max:
+                res+=left_max-height[le]
+                le+=1
+                left_max=max(left_max,height[le])
+            else:
+                res+=right_max-height[ri]
+                ri-=1
+                right_max=max(right_max,height[ri])
+        return res
 
 
