7ce1ccfb3866943d17a6ae13680d71a65f4c36a0
zhangshengm <617256843@qq.com>
2020-09-17 11:37:30 +0800

    Create jz56.py

---
+++ 经典问题/jz56.py
@@ -0,0 +1,18 @@
+class Solution:
+    #注意div为1,11,111,1111,找到的是与运算的和中中从右往左第一个为1的位置
+    #该位置的div与各个数字做与运算，可以通过判断是否为0，将数字分成两组
+    def singleNumbers(self, nums: List[int]) -> List[int]:
+        first=0
+        for i in range(len(nums)):
+            first^=nums[i]
+        div=1
+        while (div&first)==0:
+            div<<=1
+        tmp1=0
+        tmp2=0
+        for i in range(len(nums)):
+            if nums[i]&div:
+                tmp1^=nums[i]
+            else:
+                tmp2^=nums[i]
+        return [tmp1,tmp2]
