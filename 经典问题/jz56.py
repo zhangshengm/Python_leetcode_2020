bddabeb0c2d57225867efe204f41e54a79f5d969
zhangshengm <617256843@qq.com>
2020-09-17 12:20:22 +0800

    Update jz56.py

--- 经典问题/jz56.py
+++ 经典问题/jz56.py
@@ -1,5 +1,5 @@
 class Solution:
-    #注意div为1,11,111,1111,找到的是与运算的和中中从右往左第一个为1的位置
+    #注意div为1,10,100,1000,找到的是与运算的和中中从右往左第一个为1的位置
     #该位置的div与各个数字做与运算，可以通过判断是否为0，将数字分成两组
     def singleNumbers(self, nums: List[int]) -> List[int]:
         first=0
