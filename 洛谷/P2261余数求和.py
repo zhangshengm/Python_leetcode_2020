73882b019ca6468ab58826aa9a08de6b69f8d7ff
zhangshengm <617256843@qq.com>
2020-09-19 23:38:52 +0800

    Update P2261余数求和.py

--- 洛谷/P2261余数求和.py
+++ 洛谷/P2261余数求和.py
@@ -1,4 +1,7 @@
-
+# i     1  2  3  4  5   6   7   8   9   10  11 12
+# k//i 10  5  3  2  2   1   1   1   1    1  0  0
+# l=1, q=k//i, r=k//q, q*(r-l+1)*(r+l)/2
+# n*k-i*k//i, i从1到N
 import datetime
 start=datetime.datetime.now()
 N=10
