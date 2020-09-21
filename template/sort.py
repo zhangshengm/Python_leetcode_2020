6475f157c8fdb562bd8a7faab30c04d4e36b185d
zhangshengm <617256843@qq.com>
2020-09-21 14:51:56 +0800

    Update sort.py

--- template/sort.py
+++ template/sort.py
@@ -80,3 +80,14 @@ num=[4,3,1,6,7,8,12,56]
 t=heapq_sort(num,k)
 print(t)
 
+#选择排序，每轮找最小的值，放在最前面，循环遍历
+def select_sort(num):
+    if not num: return num
+    for i in range(len(num)):
+        min_index=i
+        for j in range(i+1,len(num)):
+            if num[j]<num[min_index]:
+                min_index=j
+        num[i],num[min_index]=num[min_index],num[i]
+    return num
+
