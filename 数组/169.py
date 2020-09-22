01e0eea0e354790672c5b3870d4fe44146f46367
zhangshengm <617256843@qq.com>
2020-09-22 23:20:33 +0800

    Update 169.py

--- 数组/169.py
+++ 数组/169.py
@@ -1,13 +1,20 @@
+
+#假设超过一半的元素总是存在，如果不存在只需判断maxcount_v出现的次数是否大于n/2
 class Solution:
-    #假设超过一半的元素总是存在，如果不存在只需判断maxcount_v出现的次数是否大于n/2
     def majorityElement(self, nums: List[int]) -> int:
-        maxcount_v=0
+        m_a=0
         count=0
         for i in nums:
+            #如果个数为0，初始化，个数等于一
             if count==0:
-                maxcount_v=i
-            if maxcount_v==i:
+                m_a=i
+                count=1
+                continue
+            #如果相等，个数加一
+            if m_a==i:
                 count+=1
-            else:
-                count-=1
-        return maxcount_v
+                continue
+            #如果不相等，个数减一
+            count-=1
+        return m_a
+ 
