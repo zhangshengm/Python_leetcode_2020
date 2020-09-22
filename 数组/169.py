ab6653388696c12d0910efb1712aacc4b9595207
zhangshengm <617256843@qq.com>
2020-09-22 23:30:05 +0800

    Update 169.py

--- 数组/169.py
+++ 数组/169.py
@@ -5,16 +5,15 @@ class Solution:
         m_a=0
         count=0
         for i in nums:
+            #如果相等，个数加一
+            if m_a==i:
+                count+=1
+                continue
             #如果个数为0，初始化，个数等于一
             if count==0:
                 m_a=i
                 count=1
                 continue
-            #如果相等，个数加一
-            if m_a==i:
-                count+=1
-                continue
             #如果不相等，个数减一
             count-=1
         return m_a
- 
