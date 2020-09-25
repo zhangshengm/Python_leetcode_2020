ce818c2b1835d484f37cdf9c0e2d17f6f10f0278
zhangshengm <617256843@qq.com>
2020-09-25 12:00:33 +0800

    Update string_create.py

--- template/string_create.py
+++ template/string_create.py
@@ -43,4 +43,24 @@ class Solution:
                       if dp[j][i]:
                          count+=1
        return count
-    
+
+    #无重复字符的最长子串,指针法
+    def lengthOfLongestSubstring(self, s: str) -> int:
+       if not s: return 0
+       i,h=1,set()
+       h.add(s[0])
+       start,res=0,[1]
+       while i<len(s):
+            #如果不重复，将字符加入h中，判断下一个字符
+            if s[i] not in h:
+               h.add(s[i])
+               i+=1
+            #如果重复，把现在不重复的子串长度放到res中
+            #h删除start对应的字符，start后移一位
+            else:
+               res.append(i-start)
+               h.remove(s[start])
+               start+=1
+       #保留最后一次判断的不重复子串
+       res.append(i-start)
+       return max(res)
